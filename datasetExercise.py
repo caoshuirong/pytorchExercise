0d63deb3b386913c9d158eea104fa9e19b783ce3
277273csr <2215741117@qq.com>
2020-11-04 17:12:11 +0800

    np.transpose

---
+++ datasetExercise.py
@@ -0,0 +1,31 @@
+import torch
+import torchvision
+import numpy as np
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
+import torch.utils.data as Data
+tran_set = torchvision.datasets.FashionMNIST(root = "./data",train=True,transform=transforms.Compose([transforms.ToTensor()]),download=True)
+
+tran_loader = Data.DataLoader(tran_set,10)
+
+batch = next(iter(tran_loader))
+
+images,labels = batch
+
+grid = torchvision.utils.make_grid(images,nrow=10)
+
+plt.figure(figsize=(15,15))
+
+plt.imshow(np.transpose(grid,(1,2,0)))
+
+plt.waitforbuttonpress()
+
+print("labels:",labels)
+
+
+
+
+
+
+
+
